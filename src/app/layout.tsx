f806209c51900340b0402fbcda144dc877998362
givenartwork <128499544+givenartwork@users.noreply.github.com>
2025-06-10 23:48:54 +0800

    ready to ship ✨

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Providers } from "@/provider";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+        <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        >
+            
+            <Providers>{children}</Providers>
+        </body>
     </html>
   );
 }
